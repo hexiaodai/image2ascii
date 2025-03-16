bb157503978bfad911f32d5bee711ec4b2e91f9e
hexiaodai <40925990+hexiaodai@users.noreply.github.com>
2025-03-16 15:03:35 +0800

    Update ascii.go

--- ascii/ascii.go
+++ ascii/ascii.go
@@ -28,7 +28,7 @@ type Options struct {
 
 // DefaultOptions that contains the default pixels
 var DefaultOptions = Options{
-	Pixels:   []byte(" .,:;i1tfLCG08@"),
+	Pixels:   []byte(" 01"),
 	Reversed: false,
 	Colored:  true,
 }
